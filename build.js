d484640020adc6d4eaa6f9d0929ab4bce4d122bd
Uryan Sezgin TEKE <uryan.sezgin.teke@gmail.com>
2024-03-02 15:26:30 +0300

    fix: components folder change

--- build.js
+++ build.js
@@ -1,27 +1,22 @@
 const { readdir } = require('fs/promises');
 const fs = require('fs');
-
-fs.watch('./src/assets/', main);
+const chokidar = require('chokidar');
+const root = './src/components/';
 
 /**
  *
- * @returns
  */
-const getDirectories = async () =>
-  (await readdir(`./src/assets`, { withFileTypes: true }))
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-
 function main() {
   let minify = '';
+
   getDirectories().then((dir) => {
     dir.forEach((d) => {
-      fs.readdir(`./src/assets/${d}`, (err, files) => {
+      fs.readdir(`${root}${d}`, (err, files) => {
         files.forEach(async (file) => {
           if (file.includes('js')) {
             const fileName = file.split('.')[0];
-            const jsPath = `./src/assets/${d}/${fileName}.js`;
-            const cssPath = `./src/assets/${d}/${fileName}.css`;
+            const jsPath = `${root}${d}/${fileName}.js`;
+            const cssPath = `${root}${d}/${fileName}.css`;
 
             fs.readFile(jsPath, 'utf8', (err, jsData) => {
               fs.readFile(cssPath, 'utf8', (err, cssData) => {
@@ -42,7 +37,7 @@ function main() {
   });
 
   setTimeout(() => {
-    fs.writeFile('./public/build.min.js', minify, function (err) {
+    fs.writeFile('./src/assets/build.min.js', minify, function (err) {
       if (err) {
         console.log(err);
       } else {
@@ -52,4 +47,13 @@ function main() {
   }, 3000);
 }
 
-main();
+/**
+ *
+ * @returns
+ */
+const getDirectories = async () =>
+  (await readdir(`${root}`, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+chokidar.watch(`${root}`).on('change', main);
